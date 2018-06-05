3031103498405f29aea96dffed7cf9a712d0751c
orogvany <orogvany@yahoo.com>
2018-06-04 17:07:29 -0700

    Trivial: fix imports

--- src/main/java/org/semux/gui/panel/SendPanel.java
+++ src/main/java/org/semux/gui/panel/SendPanel.java
@@ -18,8 +18,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
 
 import javax.swing.ButtonGroup;
 import javax.swing.GroupLayout;
